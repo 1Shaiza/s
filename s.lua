8e78b7531979909973eca5f72964507231b2666e
1Shaiza <154394911+1Shaiza@users.noreply.github.com>
2025-01-15 20:18:27 -0600

    Update s.lua

--- s.lua
+++ s.lua
@@ -13,6 +13,7 @@ getgenv().Settings = {
                     ["Hot Cocoa"] = {Class = "Misc", Price = "6000"},
                     ["Snowflake Gift"] = {Class = "Lootbox", Price = "10000"},
                     ["XP 1"] = {Class = "Charm", Price = "80000"},
+                    ["Huge Hunter 1"] = {Class = "Enchant", Price = "10000000"},
                     ["Charm Chisel"] = {Class = "Misc", Price = "40000"},
                     ["Rainbow Mini Chest"] = {Class = "Misc", Price = "600000"},
                     ["Mini Pinata"] = {Class = "Misc", Price = "25000"},
